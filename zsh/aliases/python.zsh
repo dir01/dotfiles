014ed351a857d81cb58ea8d622a8506e05cef3fe
Andrew Zhu <dir01@dir01.org>
2012-09-24 15:06:50 +0700

    More python-related candies

--- zsh/aliases/python.zsh
+++ zsh/aliases/python.zsh
@@ -1,3 +1,7 @@
 alias 'grepy'='grep -n --include="*.py"'
 alias 'fakemail'='sudo python -m smtpd -n -c DebuggingServer localhost:25'
+alias 'editpostactivate'='nano $VIRTUAL_ENV/bin/postactivate'
 
+function pipinstall {
+	pip install -r `find . -name requirements.txt | head -n1`
+}
